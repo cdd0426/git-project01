1fc92800bf227c7939c8981cd9289cefba8b2a28
cdd <cdd@163.com>
2023-05-05 18:17:39 +0800

    cdd master second commit!

--- src/main/java/co/zpark/ssm/App.java
+++ src/main/java/co/zpark/ssm/App.java
@@ -3,6 +3,7 @@ package co.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git cdd master second commit");
 
 
     }
